42e956cf8cf0f090b7f9ddfbf925ed5028777382
Happy BigOcean <happybigocean@gmail.com>
2025-07-20 19:37:31 -0700

    display user info

--- src/pages/Admin/Dashboard.tsx
+++ src/pages/Admin/Dashboard.tsx
@@ -5,7 +5,7 @@ export default function Dashboard() {
   return (
     <Layout>
       <div className="p-6">
-        <div className="text-xl text-gray-700 font-semibold">Dashboard</div>
+        <div className="text-xl text-gray-700 font-semibold"> Admin Dashboard</div>
         <div className="mt-4 text-gray-500">
             <p>
                 This page is under the contruction.
